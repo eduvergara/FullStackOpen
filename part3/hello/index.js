b7c0536865721d4928d30e9446d09ac211884339
Eduardo Vergara <eduvergara.dev@gmail.com>
2024-08-20 23:10:06 +1000

    uibuild update

--- part3/hello/index.js
+++ part3/hello/index.js
@@ -2,6 +2,10 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 
+app.use(cors());
+app.use(express.json());
+app.use(express.static("dist"));
+
 let notes = [
   {
     id: "1",
@@ -22,10 +26,6 @@ let notes = [
 app.get("/", (request, response) => {
   response.send("<h1>Hello World!</h1>");
 });
-
-app.use(cors());
-app.use(express.json());
-app.use(express.static("dist"));
 
 app.get("/api/notes", (request, response) => {
   response.json(notes);
