1e410c6fe5da5de4c1f4d3eb63fc3dbbf012afb6
Eduardo Vergara <eduvergara.dev@gmail.com>
2024-08-25 15:18:04 +1000

    part3 - validation

---
+++ part3/hello/mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+if (process.argv.length < 3) {
+  console.log("give password as argument");
+  process.exit(1);
+}
+
+// DO NOT SAVE THE PASSWORD TO GITHUB!!
+const password = process.argv[2];
+const url = `mongodb+srv://eduvergara:${password}@cluster0.hgi2lie.mongodb.net/noteApp?retryWrites=true&w=majority`;
+
+mongoose.set("strictQuery", false);
+mongoose.connect(url);
+
+const noteSchema = new mongoose.Schema({
+  content: String,
+  important: Boolean,
+});
+
+const Note = mongoose.model("Note", noteSchema);
+
+/*
+const note = new Note({
+  content: "GET and POST are the most important methods of HTTP protocol",
+  important: true,
+});
+
+note.save().then((result) => {
+  console.log("note saved!");
+  mongoose.connection.close();
+});
+*/
+
+Note.find({ important: true }).then((result) => {
+  result.forEach((note) => {
+    console.log(note);
+  });
+  mongoose.connection.close();
+});
