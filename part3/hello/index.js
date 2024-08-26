c3c7eba06bc1c456872eac8a3ab157dfba824ea3
Eduardo Vergara <eduvergara.dev@gmail.com>
2024-08-26 11:24:03 +1000

    part3 - lint

--- part3/hello/index.js
+++ part3/hello/index.js
@@ -1,121 +1,116 @@
-require("dotenv").config();
-const express = require("express");
-const cors = require("cors");
-const app = express();
-const Note = require("./models/note");
+require('dotenv').config()
+const express = require('express')
+const cors = require('cors')
+const app = express()
+const Note = require('./models/note')
 
-app.use(cors());
-app.use(express.json());
-app.use(express.static("dist"));
+app.use(cors())
+app.use(express.json())
+app.use(express.static('dist'))
 
 // fetching all the documents -> is it required?????
-app.get("/", (request, response) => {
-  response.send("<h1>Hello World!</h1>");
-});
+app.get('/', (request, response) => {
+  response.send('<h1>Hello World!</h1>')
+})
 
-app.get("/api/notes", (request, response) => {
+app.get('/api/notes', (request, response) => {
   Note.find({}).then((notes) => {
-    response.json(notes);
-  });
-});
+    response.json(notes)
+  })
+})
 
 // fetching a single resource - route handler
-app.get("/api/notes/:id", (request, response, next) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id)
     .then((note) => {
       if (note) {
-        response.json(note);
+        response.json(note)
       } else {
-        response.status(404).end();
+        response.status(404).end()
       }
     })
 
-    .catch((error) => {
-      (error) => next(error);
-    });
-});
+    .catch((error) => next(error)
+    )
+})
 
 // delete resources
-app.delete("/api/notes/:id", (request, response) => {
-  const id = request.params.id;
-  notes = notes.filter((note) => note.id !== id);
-  response.status(204).end();
-});
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndRemove(request.params.id)
+    .then(() => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
+})
 
-const generateId = () => {
-  const maxId =
-    notes.length > 0 ? Math.max(...notes.map((n) => Number(n.id))) : 0;
-  return String(maxId + 1);
-};
-
-app.post("/api/notes", (request, response, next) => {
-  const body = request.body;
+app.post('/api/notes', (request, response, next) => {
+  const body = request.body
 
   if (body.content === undefined) {
     return response.status(400).json({
-      error: "content missing",
-    });
+      error: 'content missing',
+    })
   }
 
   const note = new Note({
     content: body.content,
     important: body.important || false,
-  });
+  })
 
   note
     .save()
-    .then((savedNote) => {
-      response.json(note);
+    .then(() => {
+      response.json(note)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // delete note handler
-app.delete("/api/notes/:id", (request, response, next) => {
+app.delete('/api/notes/:id', (request, response, next) => {
   Note.findByIdAndDelete(request.params.id)
-    .then((result) => {
-      response.status(204).end();
+    .then(() => {
+      response.status(204).end()
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // toggling importance of note handler
-app.put("/api/notes/:id", (request, response, next) => {
-  const body = request.body;
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
 
   const note = {
     content: body.content,
     important: body.important,
-  };
+  }
 
   Note.findByIdAndUpdate(request.params.id, note, {
     new: true,
     runValidators: true,
-    context: "query",
+    context: 'query',
   })
     .then((updatedNote) => {
-      response.json(updatedNote);
+      response.json(updatedNote)
     })
-    .catch((error) => next(error));
-});
+    .catch((error) => next(error))
+})
 
 // error handler
 const errorHandler = (error, request, response, next) => {
-  console.error(error.message);
+  console.error(error.message)
 
-  if (error.name === "CastError") {
-    return response.status(400).send({ error: "malformatted id" });
-  } else if (error.name == "ValidationError") {
-    return response.status(400).send({ error: error.message });
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).send({ error: error.message })
   }
 
-  next(error);
-};
+  next(error)
+}
 
 // this has to be the last loaded middleware, also all the routes should be registered before this!
-app.use(errorHandler);
+app.use(errorHandler)
 
-const PORT = process.env.PORT;
+const PORT = process.env.PORT
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
-});
+  console.log(`Server running on port ${PORT}`)
+})
--- part3/hello/mongo.js
+++ part3/hello/mongo.js
@@ -1,23 +1,23 @@
-const mongoose = require("mongoose");
+const mongoose = require('mongoose')
 
 if (process.argv.length < 3) {
-  console.log("give password as argument");
-  process.exit(1);
+  console.log('give password as argument')
+  process.exit(1)
 }
 
 // DO NOT SAVE THE PASSWORD TO GITHUB!!
-const password = process.argv[2];
-const url = `mongodb+srv://eduvergara:${password}@cluster0.hgi2lie.mongodb.net/noteApp?retryWrites=true&w=majority`;
+const password = process.argv[2]
+const url = `mongodb+srv://eduvergara:${password}@cluster0.hgi2lie.mongodb.net/noteApp?retryWrites=true&w=majority`
 
-mongoose.set("strictQuery", false);
-mongoose.connect(url);
+mongoose.set('strictQuery', false)
+mongoose.connect(url)
 
 const noteSchema = new mongoose.Schema({
   content: String,
   important: Boolean,
-});
+})
 
-const Note = mongoose.model("Note", noteSchema);
+const Note = mongoose.model('Note', noteSchema)
 
 /*
 const note = new Note({
@@ -33,7 +33,7 @@ note.save().then((result) => {
 
 Note.find({ important: true }).then((result) => {
   result.forEach((note) => {
-    console.log(note);
-  });
-  mongoose.connection.close();
-});
+    console.log(note)
+  })
+  mongoose.connection.close()
+})
